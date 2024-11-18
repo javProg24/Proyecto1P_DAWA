09abbf47e931d84d8fd628e1f83fb0ef1b41d5cf
Peter16Xo <avilesbaque@gmail.com>
2024-11-18 08:21:15 -0500

    angular instalado

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes) // Proporciona las rutas a la aplicación
+    provideRouter(routes), provideAnimationsAsync() // Proporciona las rutas a la aplicación
   ]
 }).catch(err => console.error(err));
